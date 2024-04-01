d56f076da314d4202d3870754272b5f576602330
omar5050 <omar.saleh50505>
2024-04-01 13:45:48 +0200

    fix structure and adding register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './modules/auth/components/login/login.component';
-import { RegisterComponent } from './modules/auth/components/register/register.component';
-import { ForgotPasswrodComponent } from './modules/auth/components/forgot-passwrod/forgot-passwrod.component';
-import { ResetPasswordComponent } from './modules/auth/components/reset-password/reset-password.component';
-import { NotFoundComponent } from './modules/auth/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    ForgotPasswrodComponent,
-    ResetPasswordComponent,
-    NotFoundComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
