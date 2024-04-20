0072ad2bbcd88578ed185072307d27004f866100
Hassan <hassan.abdelrazek.hussien@outlook.com>
2024-04-20 21:37:12 +0200

    room-details-translate

--- src/app/core/service/helper.service.ts
+++ src/app/core/service/helper.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
 
+  lang:any = localStorage.getItem('lang');
+
   constructor(private _TranslateService:TranslateService) {
 
+    if (localStorage.getItem('lang') !==null) {
+       this.onChangeLanguage(this.lang)
+    }else
+    {
       this.onChangeLanguage('en')
-
+    }
+     
+    _TranslateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang
+    });
    }
 
   onChangeLanguage(lang:string) {
+
+    localStorage.setItem('lang',lang);
     // this language will be used as a fallback when a translation isn't found in the current language
     this._TranslateService.setDefaultLang(lang);
 
--- src/app/modules/landing-page/components/room-details/room-details.component.ts
+++ src/app/modules/landing-page/components/room-details/room-details.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { HelperService } from 'src/app/core/service/helper.service';
 
 const today = new Date();
 const month = today.getMonth();
@@ -10,8 +12,20 @@ const year = today.getFullYear();
   templateUrl: './room-details.component.html',
   styleUrls: ['./room-details.component.scss']
 })
-export class RoomDetailsComponent {
+export class RoomDetailsComponent implements OnInit{
 
+  lang: any = localStorage.getItem('lang');
+
+  constructor(private _HelperService:HelperService,private _TranslateService:TranslateService){
+    _TranslateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang
+    });
+  }
+
+  ngOnInit(): void {
+    
+    this.langFunc(this.lang)
+  }
   campaignOne = new FormGroup({
     start: new FormControl(new Date(year, month, 13)),
     end: new FormControl(new Date(year, month, 16)),
@@ -20,4 +34,11 @@ export class RoomDetailsComponent {
     start: new FormControl(new Date(year, month, 15)),
     end: new FormControl(new Date(year, month, 19)),
   });
+
+  langFunc(lang:string){
+    console.log(lang);
+    this._HelperService.onChangeLanguage(lang);
+    localStorage.setItem('lang', lang);
+
+  }
 }
